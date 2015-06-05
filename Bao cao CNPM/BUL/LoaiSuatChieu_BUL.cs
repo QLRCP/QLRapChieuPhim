8ac6312ed6c7426c5362f7d5ed74c90fe36edd00
nhockvt1994 <vinhlq.uit@gmail.com>
2015-06-06 00:07:40 +0700

    Revert "Revert "Quang vinh""

---
+++ Bao cao CNPM/BUL/LoaiSuatChieu_BUL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PUBLIC;
+using DAL;
+
+namespace BUL
+{
+    public class LoaiSuatChieu_BUL
+    {
+        private LoaiSuatChieu_DAL cls;
+        public LoaiSuatChieu_BUL()
+        {
+            cls = new LoaiSuatChieu_DAL();
+        }
+
+        public List<LoaiSuatChieu_Pub> GetLoaiSuatChieu()
+        {
+            return cls.GetLoaiSuatChieu();
+        }
+    }
+}
--- Bao cao CNPM/ETN_Cinema/Giao_dien_quan_ly_phim.xaml.cs
+++ Bao cao CNPM/ETN_Cinema/Giao_dien_quan_ly_phim.xaml.cs
@@ -35,5 +35,17 @@ namespace ETN_Cinema
             Giao_dien_sua_thong_tin_phim _gdsttp = new Giao_dien_sua_thong_tin_phim();
             _gdsttp.ShowDialog();
         }
+
+        private void mn_NhapSuatChieu_Click(object sender, RoutedEventArgs e)
+        {
+            Giao_dien_mo_suat_chieu _gdnsc = new Giao_dien_mo_suat_chieu();
+            _gdnsc.ShowDialog();
+        }
+
+        private void mn_SuaSuatChieu_Click(object sender, RoutedEventArgs e)
+        {
+            Giao_dien_sua_suat_chieu _gdssc = new Giao_dien_sua_suat_chieu();
+            _gdssc.ShowDialog();
+        }
     }
 }
